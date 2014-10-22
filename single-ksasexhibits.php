ea492c7592030f78fb2479a06e90e7fa9010604a
KSAS Communications <ksasweb@jhu.edu>
2014-10-22 10:21:44 -0400

    Fixed Breadcrumb Root
    Single Exhibits' breadcrumb root was 'News Archive'. Changed to
    'Exhibitions'.

--- single-ksasexhibits.php
+++ single-ksasexhibits.php
@@ -1,6 +1,24 @@
 <?php get_header(); ?>
 <div class="row wrapper radius10" id="page" role="main">
 	<div class="twelve columns radius-left offset-topgutter">	
+<?php 
+	$home_url = home_url();
+	$theme_option = flagship_sub_get_global_options();	
+	
+		if ( is_single()) { 
+			global $post;
+			$article_title = $post->post_title;
+			$article_link = $post->guid;
+		}
+		?>
+<nav role="navigation">
+			<ul id="menu-main-menu-2" class="breadcrumbs">
+				<li><a href="<?php echo $home_url; ?>">Home</a></li>
+				<li><a href="<?php echo $home_url; ?>/exhibitions">Exhibitions</a></li>
+				<li><a href="<?php echo $article_link; ?>"><?php echo $article_title; ?></a></li>
+			</ul>
+		</nav> 
+
 		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 		<section class="content">
 			<h3><?php the_title(); ?></h3>
